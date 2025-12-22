b1d9016ae686146873e7fa539766f0f2ec12aa84
maheshch07 <m.k.choudhary2606@gmail.com>
2025-12-22 18:59:14 +0530

    terraform update

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,29 +1,13 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
-  }
-}
-
-provider "aws" {
-  region = var.aws_region
-}
-
-# -----------------------------
-# SECURITY GROUP (INTENTIONALLY INSECURE)
-# -----------------------------
-resource "aws_security_group" "insecure_sg" {
-  name        = "insecure-security-group"
-  description = "Security group with intentional vulnerability"
+resource "aws_security_group" "devsecops_sg" {
+  name        = "devsecops-sg"
+  description = "Security group for DevSecOps assignment"
 
   ingress {
-    description = "SSH open to the world (INTENTIONAL VULNERABILITY)"
+    description = "SSH access"
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]  #  INSECURE ON PURPOSE
+    cidr_blocks = ["0.0.0.0/0"]   # INTENTIONALLY INSECURE
   }
 
   ingress {
@@ -42,18 +26,13 @@ resource "aws_security_group" "insecure_sg" {
   }
 }
 
-# -----------------------------
-# EC2 INSTANCE
-# -----------------------------
-resource "aws_instance" "web_server" {
-  ami           = var.ami_id
-  instance_type = "t2.micro"
-
-  vpc_security_group_ids = [
-    aws_security_group.insecure_sg.id
-  ]
+resource "aws_instance" "devsecops_ec2" {
+  ami                    = "ami-0f5ee92e2d63afc18" # Amazon Linux 2 (ap-south-1)
+  instance_type          = var.instance_type
+  key_name               = var.key_name
+  vpc_security_group_ids = [aws_security_group.devsecops_sg.id]
 
   tags = {
-    Name = "devsecops-web-server"
+    Name = "DevSecOps-Assignment"
   }
 }
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -4,7 +4,13 @@ variable "aws_region" {
   default     = "ap-south-1"
 }
 
-variable "ami_id" {
-  description = "AMI ID for EC2 instance"
+variable "instance_type" {
+  description = "EC2 instance type"
+  type        = string
+  default     = "t2.micro"
+}
+
+variable "key_name" {
+  description = "EC2 Key Pair name"
   type        = string
 }
